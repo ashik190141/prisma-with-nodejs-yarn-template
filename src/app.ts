db4bad267c25ce3ee7b31a28dc41bb090d1c8a12
ashik190141 <ashanmahmudashik@gmail.com>
2025-02-12 21:44:59 +0600

    server added with app

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,13 @@ import cors from 'cors'
 const app: Application = express();
 app.use(cors())
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req:Request, res:Response) => {
     res.send("The app is running successfully")
 })
+
+// app.use("/api/v1", route name);
 
 export default app;
